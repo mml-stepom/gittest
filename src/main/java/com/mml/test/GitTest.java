ae40299cf3ecaeb3941a77def03a6048ef3b3f77
mml <316128815@qq.com>
2022-09-22 00:11:14 +0800

    hot-fix commit 4

--- src/main/java/com/mml/test/GitTest.java
+++ src/main/java/com/mml/test/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello master branch");
         System.out.println("hello hot-fix");
+        System.out.println("2222222");
     }
     
 }
